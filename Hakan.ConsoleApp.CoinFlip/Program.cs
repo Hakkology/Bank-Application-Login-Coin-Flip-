397d571f0723c4a44a95524818a16a86619cb3f8
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-11 10:18:59 +0300

    Add files via upload

---
+++ Hakan.ConsoleApp.CoinFlip/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hakan.ConsoleApp.CoinFlip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yazı = 0;
+            int tura = 0;
+            bool devam = true;
+
+            Console.WriteLine("Dünyanın en güvenilir para atma makinesine hoşgeldiniz. Para atmak için herhangi bir tuşa basın:");
+            Console.ReadLine();
+            while (devam)
+            {
+                Console.WriteLine($"Şu ana kadar {yazı} kez yazı {tura} kez tura attınız.");
+                Random para = new Random();
+                int sonuc = para.Next(0, 5);
+                if (sonuc == 0 || sonuc == 1)
+                {
+                    Console.WriteLine("Paranız yazı geldi.");
+                    yazı++;
+                }
+                else
+                {
+                    Console.WriteLine("Paranız tura geldi.");
+                    tura++;
+                }
+                Console.WriteLine("Tekrar para atmak istiyor musunuz? (E/H): ");
+                if (Console.ReadKey().Key == ConsoleKey.E)
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Dünyanın en güvenilir para atma makinesini kullandığınız için teşekkürler.");
+                    devam = false;
+                }
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
+
+
---
+++ Hakan.ConsoleApp.Login-Register/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hakan.ConsoleApp.Login_Register
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhabalar, kanalımıza hoşgeldiniz. Kayıt yapmak için lütfen Kullanıcı adı ve Şifrenizi belirleyiniz.");
+            Console.Write("Lütfen kullanıcı adınızı yazınız: ");
+            string user = Console.ReadLine();
+            Console.Write("Lütfen şifrenizi yazınız: ");
+            string pass = Console.ReadLine();
+            //bool block = true;
+            int deneme = 3;
+
+            Console.WriteLine($"{user} adlı hesabınız oluşturulmuştur. İyi günlerde kullanın.");
+            Console.ReadLine();
+
+            //while döngüsü için bool block=true; doğru girildiği zaman while=false.
+            #region While
+            //while (deneme <3)
+            //{
+            //    Console.WriteLine("Lütfen kanalımıza hesap bilgileriniz ile giriş yapınız: ");
+            //    Console.WriteLine("Kullanıcı adınızı giriniz: ");
+            //    string username = Console.ReadLine();
+            //    Console.WriteLine("Şifrenizi giriniz: ");
+            //    string password = Console.ReadLine();
+            //    if (username == user && password == pass)
+            //    { 
+            //        block = false;
+            //        Console.WriteLine("Evinize hoşgeldiniz. İyi eğlenceler.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Hesap bilgileriniz yanlış, lütfen tekrar giriniz.");
+            //    }
+            //Console.ReadLine();
+            //} 
+            #endregion
+
+            //for döngüsü için giriş sayısı 3ü geçmemeli.
+            for (int giriş = 0; giriş < 3; giriş++)
+            {
+                Console.WriteLine("Lütfen kanalımıza hesap bilgileriniz ile giriş yapınız.");
+                Console.Write("Kullanıcı adınızı giriniz: ");
+                string username = Console.ReadLine();
+                Console.Write("Şifrenizi giriniz: ");
+                string password = Console.ReadLine();
+                if (username == user && password == pass)
+                {
+                    Console.WriteLine("Kanalımıza hoşgeldiniz. İyi eğlenceler.");
+                    Console.ReadLine();
+                    break;
+                }
+                else if (deneme == 1)
+                {
+                    Console.WriteLine("Hesabınız yanlış girişten dolayı bloklanmıştır. İyi günler.");
+                }
+                else
+                {
+                    deneme = 2 - giriş;
+                    Console.WriteLine($"Yanlış bir giriş yaptınız, tekrar denemek için herhangi bir tuşa basınız. Bloklanmadan önce {deneme} denemeniz kaldı.");
+                }
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
